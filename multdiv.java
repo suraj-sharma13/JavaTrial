94eadaa059f7945798900b58bf169da3cd1c5e0a
suraj-sharma13 <sharmasuraj1303@gmail.com>
2023-01-26 01:04:27 +0530

    added a new file multdiv

---
+++ multdiv.java
@@ -0,0 +1,14 @@
+public class Main{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+	public static int multiplyNum(int a, int b){
+		int c = a * b;
+		return c;
+	}
+
+	public static int divideNum(int a, int b){
+		int c = a / b;
+		return c;
+	}
+}
